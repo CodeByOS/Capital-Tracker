27c4dd3ad994b6d1d211ac10656635c24ddceb30
Oussama <saidi.oussama2004@gmail.com>
2025-06-28 22:14:47 +0100

    Create Transaction Action..

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,10 +1,12 @@
+import { getUserAccounts } from "@/actions/dashboard"
 import AddTransactionForm from "../_components/transactionForm"
 
 
-const TransactionPage = () => {
+const TransactionPage = async () => {
+  const accounts = await getUserAccounts();
   return (
     <div className="max-w-3xl mx-auto px-5">
-      <h1>Create Transaction</h1>
+      <h1 className="text-5xl font-bold mb-5 bg-gradient-to-r from-purple-800 to-blue-800 bg-clip-text text-transparent inline-block">Create Transaction</h1>
       <AddTransactionForm
         accounts={accounts}
         categories={defaultCategories}
