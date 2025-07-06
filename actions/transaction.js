313af5913ad7ec6b7378e2a76dc2c3cdb23b3aec
Oussama <saidi.oussama2004@gmail.com>
2025-07-06 16:56:20 +0100

    Transaction Actions..

--- actions/transaction.js
+++ actions/transaction.js
@@ -140,7 +140,7 @@ export async function scanReceipt(file) {
                 "category": "string"
             }
 
-            If its not a recipt, return an empty object
+            If its not a recipt, return an empty object.
         `
 
         const result = await model.generateContent([
