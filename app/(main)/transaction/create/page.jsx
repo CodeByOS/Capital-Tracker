6e32ac814f7ad690ec23b7d6e26452300270aac8
Oussama <saidi.oussama2004@gmail.com>
2025-07-06 19:56:33 +0100

    Transaction Page..

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,16 +1,28 @@
 import { getUserAccounts } from "@/actions/dashboard"
 import { defaultCategories } from "@/data/categories";
 import { AddTransactionForm } from "../_components/transactionForm";
+import { getTransaction } from "@/actions/transaction";
 
 
-const TransactionPage = async () => {
+const TransactionPage = async ({ searchParams }) => {
   const accounts = await getUserAccounts();
+  const editId = searchParams.editId || null;
+
+  // console.log(editId)
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
   return (
     <div className="max-w-3xl mx-auto px-5">
       <h1 className="text-5xl font-bold mb-5 bg-gradient-to-r from-purple-800 to-blue-800 bg-clip-text text-transparent inline-block">Create Transaction</h1>
       <AddTransactionForm
         accounts={accounts}
         categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
       />
     </div>
   )
