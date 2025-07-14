964f2e9e573ac43d53b0dc255d16331752c628eb
Oussama <saidi.oussama2004@gmail.com>
2025-07-14 12:51:21 +0100

    Update next js config..

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "randomuser.me",
-            }
-        ]
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**", // <-- this is the missing piece
+      },
+    ],
+  },
 
-    experimental: {
-        serverActions: {
-            bodySizeLimit: "5mb", // Increase the body size limit for server actions
-        }
-    }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
